f803d565f5ed199a76a6200833702d03bf5db184
Robert Dyro <rdyro@stanford.edu>
2023-04-16 14:54:58 -0700

    flax version, takes forever to compile

--- darts_jax/cnn/model_search.py
+++ darts_jax/cnn/model_search.py
@@ -11,186 +11,132 @@ from .genotypes import Genotype
 import numpy as np
 
 from jfi import jaxm, make_random_key as mrk
-import equinox as eqx
-from equinox import nn
+from flax import linen as nn
+import flax
+from jax.tree_util import tree_map
 
 Array = jaxm.jax.Array
 
 
-class MixedOp(eqx.Module):
-    _ops: List[eqx.Module]
+class MixedOp(nn.Module):
+    C: int
+    stride: int
 
-    def __init__(self, C, stride):
-        self._ops = []
+    def setup(self):
+        _ops = []
         for primitive in PRIMITIVES:
-            op = OPS[primitive](C, stride, False)
+            op = OPS[primitive](self.C, self.stride, False)
             if "pool" in primitive:
-                op = Sequential([op, BatchNorm2d(C, affine=False)])
-            self._ops.append(op)
+                op = nn.Sequential([op, BatchNorm2d(self.C, affine=False)])
+            _ops.append(op)
+        self._ops = _ops
 
-    def __call__(self, x, state=None, weights=None):
+    def __call__(self, x, weights):
         # return sum(w * op(x) for w, op in zip(weights, self._ops))
         assert weights is not None
-        outputs = [op(x, state=state) for op in self._ops]
-        xs, states = zip(
-            *[(out[0], out[1]) if isinstance(out, tuple) else (out[0], dict()) for out in outputs]
-        )
-        x = sum(w * x for w, x in zip(weights, xs))
-        state = dict()
-        for state_ in states:
-            state.update(state_)
-        return x, state
+        xs = [op(x) for op in self._ops]
+        x = sum(w * x for (w, x) in zip(weights, xs))
+        return x
 
 
-class Cell(eqx.Module):
-    preprocess0: Union[ReLUConvBN, FactorizedReduce]
-    preprocess1: ReLUConvBN
-    _steps: int
-    _multiplier: int
-    _ops: List[eqx.Module]
-    reduction: bool
-    C: int
-    C_prev: int
+class Cell(nn.Module):
+    steps: int
+    multiplier: int
     C_prev_prev: int
+    C_prev: int
+    C: int
+    reduction: bool
+    reduction_prev: bool
 
-    def __init__(self, steps, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev):
-        self.reduction = reduction
-
-        if reduction_prev:
-            self.preprocess0 = FactorizedReduce(C_prev_prev, C, affine=False)
+    def setup(self):
+        if self.reduction_prev:
+            self.preprocess0 = FactorizedReduce(self.C_prev_prev, self.C, affine=False)
         else:
-            self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0, affine=False)
-        self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0, affine=False)
-        self._steps = steps
-        self._multiplier = multiplier
-        self.C = C
-        self.C_prev = C_prev
-        self.C_prev_prev = C_prev_prev
+            self.preprocess0 = ReLUConvBN(self.C_prev_prev, self.C, 1, 1, 0, affine=False)
+        self.preprocess1 = ReLUConvBN(self.C_prev, self.C, 1, 1, 0, affine=False)
 
-        self._ops = []
-        for i in range(self._steps):
+        _ops = []
+        for i in range(self.steps):
             for j in range(2 + i):
-                stride = 2 if reduction and j < 2 else 1
-                op = MixedOp(C, stride)
-                self._ops.append(op)
+                stride = 2 if self.reduction and j < 2 else 1
+                op = MixedOp(self.C, stride)
+                _ops.append(op)
+        self._ops = _ops
 
-    def __call__(self, s0, s1, weights, state=None):
-        state = dict() if state is None else copy(state)
-        s0, state = self.preprocess0(s0, state=state)
-        s1, state = self.preprocess1(s1, state=state)
+    def __call__(self, s0, s1, weights):
+        s0 = self.preprocess0(s0)
+        s1 = self.preprocess1(s1)
 
         outputs = [s0, s1]
         offset = 0
-        for i in range(self._steps):
-            # ss, states = zip(*[self._ops[offset + j](h, weights=weights[offset + j], state=state) for j, h in enumerate(states)])
-            rets = [
-                self._ops[offset + j](h, weights=weights[offset + j], state=state)
-                for j, h in enumerate(outputs)
+        for i in range(self.steps):
+            ss = [
+                self._ops[offset + j](h, weights=weights[offset + j]) for j, h in enumerate(outputs)
             ]
-            ss, states = zip(
-                *[(ret[0], ret[1]) if isinstance(ret, tuple) else (ret[0], dict()) for ret in rets]
-            )
             s = sum(ss)
-            assert not isinstance(s, tuple)
-            for state_ in states:
-                state.update(state_)
             offset += len(outputs)
             outputs.append(s)
 
-        x = jaxm.cat(outputs[-self._multiplier :], axis=-3)
-        assert x.shape[-3] == self.C * self._multiplier
-        return x, state
+        x = jaxm.cat(outputs[-self.multiplier :], axis=-3)
+        assert x.shape[-3] == self.C * self.multiplier
+        return x
 
 
-class Network(eqx.Module):
-    _C: int
-    _num_classes: int
-    _layers: int
-    _criterion: Callable
-    _steps: int
-    _multiplier: int
-    cells: List[eqx.Module]
-    alphas_normal: Array
-    alphas_reduce: Array
-    _arch_parameters: List[Array]
-    stem: Sequential
-    global_pooling: AdaptiveAvgPool2d
-    classifier: Linear
+class Network(nn.Module):
+    C: int
+    num_classes: int
+    layers: int
+    steps: int = 4
+    multiplier: int = 4
+    stem_multiplier: int = 3
 
-    def __init__(
-        self,
-        C,
-        num_classes,
-        layers,
-        criterion: Optional[Callable] = None,
-        steps=4,
-        multiplier=4,
-        stem_multiplier=3,
-    ):
-        self._C = C
-        self._num_classes = num_classes
-        self._layers = layers
-        self._criterion = criterion
-        self._steps = steps
-        self._multiplier = multiplier
-
-        C_curr = stem_multiplier * C
-        self.stem = Sequential(
-            [Conv2d(3, C_curr, 3, padding=1, use_bias=False, key=mrk()), BatchNorm2d(C_curr)]
+    def setup(self):
+        C_curr = self.stem_multiplier * self.C
+        self.stem = nn.Sequential(
+            [Conv2d(3, C_curr, 3, padding=1, use_bias=False), BatchNorm2d(C_curr)]
         )
 
-        C_prev_prev, C_prev, C_curr = C_curr, C_curr, C
-        self.cells = []
+        C_prev_prev, C_prev, C_curr = C_curr, C_curr, self.C
+        cells = []
         reduction_prev = False
-        for i in range(layers):
-            if i in [layers // 3, 2 * layers // 3]:
+        for i in range(self.layers):
+            if i in [self.layers // 3, 2 * self.layers // 3]:
                 C_curr *= 2
                 reduction = True
             else:
                 reduction = False
-            cell = Cell(steps, multiplier, C_prev_prev, C_prev, C_curr, reduction, reduction_prev)
+            cell = Cell(
+                self.steps, self.multiplier, C_prev_prev, C_prev, C_curr, reduction, reduction_prev
+            )
             reduction_prev = reduction
-            self.cells += [cell]
-            C_prev_prev, C_prev = C_prev, multiplier * C_curr
+            cells.append(cell)
+            C_prev_prev, C_prev = C_prev, self.multiplier * C_curr
+        self.cells = cells
 
         self.global_pooling = AdaptiveAvgPool2d(1)
-        self.classifier = Linear(C_prev, num_classes, key=mrk())
+        self.classifier = Linear(self.num_classes)
 
         self._initialize_alphas()
 
-    def init_state(self):
-        dtype = self.classifier.weight.dtype
-        device = self.classifier.weight.device()
-        x = jaxm.ones((2, 3, 4, 4), dtype=dtype, device=device)
-        _, state = self.__call__(x)
-        return state
-
-    def new(self):
-        model_new = Network(self._C, self._num_classes, self._layers, self._criterion).cuda()
-        for x, y in zip(model_new.arch_parameters(), self.arch_parameters()):
-            x.data.copy_(y.data)
-        return model_new
-
-    def __call__(self, input, state=None):
-        state = dict() if state is None else copy(state)
-        s0, state = self.stem(input, state=state)
+    def __call__(self, input):
+        s0 = self.stem(input)
         s1 = s0
         for i, cell in enumerate(self.cells):
             if cell.reduction:
                 weights = jaxm.softmax(self.alphas_reduce, axis=-1)
             else:
                 weights = jaxm.softmax(self.alphas_normal, axis=-1)
-            (s0, (s1, state)) = s1, cell(s0, s1, weights, state=state)
-        out = self.global_pooling(s1, state=state)
-        logits = self.classifier(out.reshape(out.shape[:-3] + (-1,)), state=state)
-        return logits, state
+            (s0, s1) = s1, cell(s0, s1, weights)
+        out = self.global_pooling(s1)
+        logits = self.classifier(out.reshape(out.shape[:-3] + (-1,)))
+        return logits
 
     def _loss(self, input, target):
         logits = self(input)
         return self._criterion(logits, target)
 
     def _initialize_alphas(self):
-        k = sum(1 for i in range(self._steps) for n in range(2 + i))
+        k = sum(1 for i in range(self.steps) for n in range(2 + i))
         num_ops = len(PRIMITIVES)
 
         # self.alphas_normal = Variable(1e-3*torch.randn(k, num_ops).cuda(), requires_grad=True)
--- darts_jax/cnn/operations.py
+++ darts_jax/cnn/operations.py
@@ -1,286 +1,253 @@
-from typing import List, Callable, Optional, Union
+from typing import List, Callable, Optional, Union, Tuple
 from copy import copy
 
 # import torch
 
 from jfi import jaxm, make_random_key as mrk
-
-import equinox as eqx
-from equinox import nn
-from equinox.experimental import BatchNorm
+from flax import linen as nn
 
 from jax import Array
 
 batch_wrapper = lambda x: jaxm.vmap(x)
 
-class ReLU(eqx.Module):
-    def __call__(self, x, *args, **kw):
+
+class ReLU(nn.Module):
+    def __call__(self, x):
         return jaxm.maximum(x, 0)
 
+
 ####################################################################################################
 
 
-class BatchNorm2d(eqx.Module):
-    momentum: float = 0.1
-    gamma: Array
-    beta: Array
-    affine: bool
+class BatchNorm2d(nn.Module):
     in_channels: int
-    hash: int
-    eps: float
+    affine: bool = False
 
-    def __init__(self, in_channels, affine=False, momentum=0.1, eps=1e-5):
-        self.in_channels = in_channels
-        self.momentum = momentum
-        self.affine = affine
-        self.gamma, self.beta = jaxm.ones(1), jaxm.zeros(1)
-        self.hash = id(self)
-        self.eps = eps
+    def setup(self):
+        self.op = nn.BatchNorm(use_running_average=True, axis=-3)
 
-    def init_state(self):
-        running_mean = jaxm.zeros(self.in_channels)
-        running_var = jaxm.ones(self.in_channels)
-        return {
-            f"{self.hash}_running_mean": running_mean,
-            f"{self.hash}_running_var": running_var,
-        }
-
-    def __call__(self, x, state=None):
-        if state is None:
-            state = self.init_state()
-        if f"{self.hash}_running_mean" not in state or f"{self.hash}_running_var" not in state:
-            state = dict(state, **self.init_state())
-        running_mean = state[f"{self.hash}_running_mean"]
-        running_var = state[f"{self.hash}_running_var"]
-
-        axis = tuple(-i for i in range(1, x.ndim + 1) if i != -3)
-        mean = jaxm.mean(x, axis=axis)
-        var = jaxm.var(x, axis=axis)
-        running_mean = (1 - self.momentum) * running_mean + self.momentum * mean
-        running_var = (1 - self.momentum) * running_var + self.momentum * var
-        state[f"{self.hash}_running_mean"] = running_mean
-        state[f"{self.hash}_running_var"] = running_var
-
-        mean, var = running_mean[:, None, None], running_var[:, None, None]
-        if self.affine:
-            x = (x - mean) / jaxm.sqrt(var + self.eps) * self.gamma + self.beta
-        else:
-            x = (x - mean) / jaxm.sqrt(var + self.eps)
-        return x, state
+    def __call__(self, x):
+        return self.op(x)
 
 
 ####################################################################################################
 
 
-class Sequential(eqx.Module):
-    mod_list: List[eqx.Module]
+class Conv2d(nn.Module):
+    C_in: int
+    C_out: int
+    kernel_size: Union[int, Tuple[int, int]]
+    stride: Union[int, Tuple[int, int]] = 1
+    padding: Union[int, Tuple[int, int], str] = "SAME"
+    dilation: int = 1
+    groups: int = 1
+    use_bias: bool = True
 
-    def __init__(self, mod_list: List[eqx.Module]):
-        self.mod_list = mod_list
+    def setup(self):
+        kernel_size = (
+            (self.kernel_size,) * 2 if isinstance(self.kernel_size, int) else self.kernel_size
+        )
+        padding = (self.padding,) * 2 if isinstance(self.padding, int) else self.padding
+        stride = (self.stride,) * 2 if isinstance(self.stride, int) else self.stride
 
-    def init_state(self):
-        state = dict()
-        for mod in self.mod_list:
-            if hasattr(mod, "init_state"):
-                state = dict(state, **mod.init_state())
-        return state
+        self.op = nn.Conv(
+            self.C_out,
+            kernel_size,
+            stride,
+            padding,
+            kernel_dilation=self.dilation,
+            feature_group_count=self.groups,
+            use_bias=self.use_bias,
+        )
 
-    def __call__(self, x, state=None):
-        total_state = dict() if state is None else copy(state)
-        for mod in self.mod_list:
-            out = mod(x, state=total_state)
-            if isinstance(out, tuple):
-                x, state = out
-                total_state = dict(total_state, **state)
-            else:
-                x = out
-        return x, total_state
-
-
-class Conv2d(eqx.Module):
-    op: nn.Conv2d
-
-    def __init__(self, *args, **kw):
-        self.op = nn.Conv2d(*args, **kw)
-
-    def __call__(self, x, state=None):
-        return batch_wrapper(self.op)(x)
+    def __call__(self, x):
+        return self.op(jaxm.swapaxes(x, -1, -3)).swapaxes(-1, -3)
 
 
-class AdaptiveAvgPool2d(eqx.Module):
-    op: nn.AdaptiveAvgPool2d
+class AdaptiveAvgPool2d(nn.Module):
+    output_size: Union[int, Tuple[int, int]]
 
-    def __init__(self, *args, **kw):
-        self.op = nn.AdaptiveAvgPool2d(*args, **kw)
+    def setup(self):
+        assert self.output_size == 1 or self.output_size == (1, 1)
 
-    def __call__(self, x, state=None):
-        return batch_wrapper(self.op)(x)
-
-
-class Linear(eqx.Module):
-    op: nn.Linear
-
-    def __init__(self, *args, **kw):
-        self.op = nn.Linear(*args, **kw)
-
-    def __call__(self, x, state=None):
-        return batch_wrapper(self.op)(x)
+    def __call__(self, x):
+        return jaxm.mean(x, (-1, -2), keepdims=True)
 
 
-class MaxPool2d(eqx.Module):
-    op: nn.MaxPool2d
+class Linear(nn.Module):
+    out_features: int
 
-    def __init__(self, *args, **kw):
-        self.op = nn.MaxPool2d(*args, **kw)
+    def setup(self):
+        self.op = nn.Dense(self.out_features)
 
-    def __call__(self, x, state=None):
-        return batch_wrapper(self.op)(x)
+    def __call__(self, x):
+        return self.op(x)
 
 
-class AvgPool2d(eqx.Module):
-    op: nn.AvgPool2d
+class MaxPool2d(nn.Module):
+    window: int
+    stride: int
+    padding: str
 
-    def __init__(self, *args, **kw):
-        self.op = nn.AvgPool2d(*args, **kw)
+    def __call__(self, x):
+        x = jaxm.swapaxes(x, -3, -1)
+        x = nn.max_pool(x, (self.window, self.window), (self.stride, self.stride), self.padding)
+        return jaxm.swapaxes(x, -3, -1)
 
-    def __call__(self, x, state=None):
-        return batch_wrapper(self.op)(x)
+
+class AvgPool2d(nn.Module):
+    window: int
+    stride: int
+    padding: str
+
+    def __call__(self, x):
+        x = jaxm.swapaxes(x, -3, -1)
+        x = nn.avg_pool(x, (self.window, self.window), (self.stride, self.stride), self.padding)
+        return jaxm.swapaxes(x, -3, -1)
 
 
 ####################################################################################################
 
 
-class ReLUConvBN(eqx.Module):
-    op: Sequential
+class ReLUConvBN(nn.Module):
+    C_in: int
+    C_out: int
+    kernel_size: Union[int, Tuple[int, int]]
+    stride: Union[int, Tuple[int, int]]
+    padding: Union[int, Tuple[int, int]]
+    affine: bool = True
 
-    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
-        self.op = Sequential(
+    def setup(self):
+        self.op = nn.Sequential(
             [
                 ReLU(),
                 Conv2d(
-                    C_in,
-                    C_out,
-                    kernel_size,
-                    stride=stride,
-                    padding=padding,
+                    self.C_in,
+                    self.C_out,
+                    self.kernel_size,
+                    stride=self.stride,
+                    padding=self.padding,
                     use_bias=False,
-                    key=mrk(),
                 ),
-                BatchNorm2d(C_out, affine=affine),
+                BatchNorm2d(self.C_out, affine=self.affine),
             ]
         )
 
-    def __call__(self, x, *args, **kw):
-        return self.op(x, *args, **kw)
+    def __call__(self, x):
+        return self.op(x)
 
 
-class DilConv(eqx.Module):
-    op: Sequential
+class DilConv(nn.Module):
+    C_in: int
+    C_out: int
+    kernel_size: Union[int, Tuple[int, int]]
+    stride: Union[int, Tuple[int, int]]
+    padding: Union[int, Tuple[int, int]]
+    dilation: Union[int, Tuple[int, int]]
+    affine: bool = True
 
-    def __init__(self, C_in, C_out, kernel_size, stride, padding, dilation, affine=True):
-        self.op = Sequential(
+    def setup(self):
+        self.op = nn.Sequential(
             [
                 ReLU(),
                 Conv2d(
-                    C_in,
-                    C_in,
-                    kernel_size=kernel_size,
-                    stride=stride,
-                    padding=padding,
-                    dilation=dilation,
-                    groups=C_in,
+                    self.C_in,
+                    self.C_in,
+                    kernel_size=self.kernel_size,
+                    stride=self.stride,
+                    padding=self.padding,
+                    dilation=self.dilation,
+                    groups=self.C_in,
                     use_bias=False,
-                    key=mrk(),
                 ),
-                Conv2d(C_in, C_out, kernel_size=1, padding=0, use_bias=False, key=mrk()),
-                BatchNorm2d(C_out, affine=affine),
+                Conv2d(self.C_in, self.C_out, kernel_size=1, padding=0, use_bias=False),
+                BatchNorm2d(self.C_out, affine=self.affine),
             ]
         )
 
-    def __call__(self, x, *args, **kw):
-        return self.op(x, *args, **kw)
+    def __call__(self, x):
+        return self.op(x)
 
 
-class SepConv(eqx.Module):
-    op: Sequential
+class SepConv(nn.Module):
+    C_in: int
+    C_out: int
+    kernel_size: Union[int, Tuple[int, int]]
+    stride: Union[int, Tuple[int, int]]
+    padding: Union[int, Tuple[int, int]]
+    affine: bool = True
 
-    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
-        self.op = Sequential(
+    def setup(self):
+        self.op = nn.Sequential(
             [
                 ReLU(),
                 Conv2d(
-                    C_in,
-                    C_in,
-                    kernel_size=kernel_size,
-                    stride=stride,
-                    padding=padding,
-                    groups=C_in,
+                    self.C_in,
+                    self.C_in,
+                    kernel_size=self.kernel_size,
+                    stride=self.stride,
+                    padding=self.padding,
+                    groups=self.C_in,
                     use_bias=False,
-                    key=mrk(),
                 ),
-                Conv2d(C_in, C_in, kernel_size=1, padding=0, use_bias=False, key=mrk()),
-                BatchNorm2d(C_in, affine=affine),
+                Conv2d(self.C_in, self.C_in, kernel_size=1, padding=0, use_bias=False),
+                BatchNorm2d(self.C_in, affine=self.affine),
                 ReLU(),
                 Conv2d(
-                    C_in,
-                    C_in,
-                    kernel_size=kernel_size,
+                    self.C_in,
+                    self.C_in,
+                    kernel_size=self.kernel_size,
                     stride=1,
-                    padding=padding,
-                    groups=C_in,
+                    padding=self.padding,
+                    groups=self.C_in,
                     use_bias=False,
-                    key=mrk(),
                 ),
-                Conv2d(C_in, C_out, kernel_size=1, padding=0, use_bias=False, key=mrk()),
-                BatchNorm2d(C_out, affine=affine),
+                Conv2d(self.C_in, self.C_out, kernel_size=1, padding=0, use_bias=False),
+                BatchNorm2d(self.C_out, affine=self.affine),
             ]
         )
 
-    def __call__(self, x, *args, **kw):
-        return self.op(x, *args, **kw)
+    def __call__(self, x):
+        return self.op(x)
 
 
-class Identity(eqx.Module):
-    def __call__(self, x, *args, **kw):
+class Identity(nn.Module):
+    def __call__(self, x):
         return x
 
 
-class Zero(eqx.Module):
+class Zero(nn.Module):
     stride: int
 
-    def __init__(self, stride):
-        self.stride = stride
-
-    def __call__(self, x, *args, **kw):
+    def __call__(self, x):
         if self.stride == 1:
             return 0 * x
         return 0 * x[..., :: self.stride, :: self.stride]
 
 
-class FactorizedReduce(eqx.Module):
-    relu: ReLU
-    conv_1: Conv2d
-    conv_2: Conv2d
-    bn: BatchNorm2d
+class FactorizedReduce(nn.Module):
+    C_in: int
+    C_out: int
+    affine: bool = True
 
-    def __init__(self, C_in, C_out, affine=True):
-        assert C_out % 2 == 0
+    def setup(self):
+        assert self.C_out % 2 == 0
         self.relu = ReLU()
-        self.conv_1 = Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, use_bias=False, key=mrk())
-        self.conv_2 = Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, use_bias=False, key=mrk())
-        self.bn = BatchNorm2d(C_out, affine=affine)
+        self.conv_1 = Conv2d(self.C_in, self.C_out // 2, 1, stride=2, padding=0, use_bias=False)
+        self.conv_2 = Conv2d(self.C_in, self.C_out // 2, 1, stride=2, padding=0, use_bias=False)
+        self.bn = BatchNorm2d(self.C_out, affine=self.affine)
 
-    def __call__(self, x, state=None):
+    def __call__(self, x):
         x = self.relu(x)
-        out = jaxm.cat([self.conv_1(x), self.conv_2(x[..., :, 1:, 1:])], axis=-3)
-        out, state = self.bn(out, state=state)
-        return out, state
+        x = jaxm.cat([self.conv_1(x), self.conv_2(x[..., :, 1:, 1:])], axis=-3)
+        x = self.bn(x)
+        return x
 
 
 OPS = {
     "none": lambda C, stride, affine: Zero(stride),
-    "avg_pool_3x3": lambda C, stride, affine: AvgPool2d(3, stride=stride, padding=1),
-    "max_pool_3x3": lambda C, stride, affine: MaxPool2d(3, stride=stride, padding=1),
+    "avg_pool_3x3": lambda C, stride, affine: AvgPool2d(3, stride=stride, padding="SAME"),
+    "max_pool_3x3": lambda C, stride, affine: MaxPool2d(3, stride=stride, padding="SAME"),
     "skip_connect": lambda C, stride, affine: Identity()
     if stride == 1
     else FactorizedReduce(C, C, affine=affine),
@@ -289,12 +256,12 @@ OPS = {
     "sep_conv_7x7": lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
     "dil_conv_3x3": lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
     "dil_conv_5x5": lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
-    "conv_7x1_1x7": lambda C, stride, affine: Sequential(
+    "conv_7x1_1x7": lambda C, stride, affine: nn.Sequential(
         [
             ReLU(),
-            Conv2d(C, C, (1, 7), stride=(1, stride), padding=(0, 3), use_bias=False, key=mrk()),
-            Conv2d(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), use_bias=False, key=mrk()),
-            BatchNorm(C, affine=affine),
+            Conv2d(C, C, (1, 7), stride=(1, stride), padding=(0, 3), use_bias=False),
+            Conv2d(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), use_bias=False),
+            BatchNorm2d(C, affine=affine),
         ]
     ),
 }
